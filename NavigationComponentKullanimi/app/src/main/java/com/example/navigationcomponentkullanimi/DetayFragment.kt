70e362be518c451ecd1f17b3143b30c8dd665f25
Erol Turkkan <b3tdeveloper@gmail.com>
2025-05-03 15:51:02 +0300

    Bottom Navigation

--- NavigationComponentKullanimi/app/src/main/java/com/example/navigationcomponentkullanimi/DetayFragment.kt
+++ NavigationComponentKullanimi/app/src/main/java/com/example/navigationcomponentkullanimi/DetayFragment.kt
@@ -1,10 +1,12 @@
 package com.example.navigationcomponentkullanimi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.navArgs
 import com.example.navigationcomponentkullanimi.databinding.FragmentDetayBinding
 
@@ -21,6 +23,16 @@ class DetayFragment : Fragment() {
         val gelenSayi = bundle.sayi
 
         binding.textViewSonuc.text = "$gelenMesaj - $gelenSayi"
+
+        val geriTusu = object : OnBackPressedCallback(true){//true: geri dönüş aktif değil
+            override fun handleOnBackPressed() {
+                // Geri tuşuna basıldığında yapılacak işlemler
+                Log.e("Detay Sayfa", "Geri Tuşuna Basıldı")
+                isEnabled = false
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, geriTusu)
 
         return binding.root
     }
