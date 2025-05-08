41a774118e70c2f32eb3da74454856d8d78fac4e
Erol Turkkan <b3tdeveloper@gmail.com>
2025-05-08 21:13:28 +0300

    Kişiler Uygulaması Temel Tasarım

--- KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,14 +1,19 @@
 package com.example.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.kisileruygulamasi.R
 import com.example.kisileruygulamasi.data.entity.Kisiler
 import com.example.kisileruygulamasi.databinding.FragmentAnasayfaBinding
+import com.example.kisileruygulamasi.ui.adapter.KisilerAdapter
 
 class AnasayfaFragment : Fragment() {
 
@@ -24,12 +29,41 @@ class AnasayfaFragment : Fragment() {
             Navigation.findNavController(it).navigate(R.id.kisiKayitGecis)
         }
 
-        binding.buttonDetay.setOnClickListener {
-            val kisi = Kisiler(1, "Erol", "123")
-            val gecis = AnasayfaFragmentDirections.kisiDetayGecis(kisi = kisi)
-            Navigation.findNavController(it).navigate(gecis)
-        }
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1, "Ahmet", "999999999")
+        val k2 = Kisiler(2, "Mehmet", "888888888")
+        val k3 = Kisiler(3, "Ali", "777777777")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+
+        val kisilerAdapter = KisilerAdapter(requireContext(), kisilerListesi)
+        binding.kisilerRv.adapter = kisilerAdapter
+
+        binding.kisilerRv.layoutManager = LinearLayoutManager(requireContext())
+        //binding.kisilerRv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        //binding.kisilerRv.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                ara(newText)
+                return true
+            }
+            override fun onQueryTextSubmit(query: String): Boolean {
+                ara(query)
+                return false
+            }
+        })
+
 
         return binding.root
+    }
+    fun ara(aramaKelimesi:String){
+        Log.e("Kişi Ara", aramaKelimesi)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("Kişi Anasayfaya", "Dönüldü")
     }
 }
